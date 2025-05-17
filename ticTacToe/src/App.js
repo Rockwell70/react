177469cb70019451de150734dc4febeef3ebdd68
Rockwell70 <rochwellshabani@gmail.com>
2025-05-16 17:19:32 -0700

    added history tracking to game

--- ticTacToe/src/App.js
+++ ticTacToe/src/App.js
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
 }
 
 
-export default function Board() {
+function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -80,4 +80,20 @@ function calculateWinner(squares) {
         }
     }
     return null;
+}
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length-1];
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    );
 }
